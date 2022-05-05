a666bce5560b084bd0656f4726ab746a1da2a9da
IP-115-Popov <serzh.serp123.popov@mail.ru>
2022-05-05 20:32:21 +0700

    Src files

---
+++ src/libgeo/area.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include "area.h"
+
+
+float areaCircle(int r)
+{
+	return r*r*3.1415926535;
+}
+float areaTriangle(int ax, int ay, int bx, int by, int cx, int cy)
+{
+	return 0.5*(ax-cx)*(by-cy)-(bx-cx)*(ay-cy);
+}
