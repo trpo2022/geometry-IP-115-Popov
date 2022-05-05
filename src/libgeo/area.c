9663c027bd42117244235f54629f20bff108cc15
IP-115-Popov <serzh.serp123.popov@mail.ru>
2022-05-05 20:38:45 +0700

    Formatting for build

--- src/libgeo/area.c
+++ src/libgeo/area.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <math.h>
 #include "area.h"
-
+#include <math.h>
+#include <stdio.h>
 
 float areaCircle(int r)
 {
-	return r*r*3.1415926535;
+    return r * r * 3.1415926535;
 }
 float areaTriangle(int ax, int ay, int bx, int by, int cx, int cy)
 {
-	return 0.5*(ax-cx)*(by-cy)-(bx-cx)*(ay-cy);
+    return 0.5 * (ax - cx) * (by - cy) - (bx - cx) * (ay - cy);
 }
--- src/libgeo/touchCircles.c
+++ src/libgeo/touchCircles.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <math.h>
 #include "touchCircles.h"
+#include <math.h>
+#include <stdio.h>
 
 int touchCircles(int x1, int y1, int r1, int x2, int y2, int r2)
 {
-	int l;
-	l = sqrt(powf(fabs((float)(x1-x2)), 2) + powf(fabs((float)(y1-y2)), 2));
-	if (l <= r1+r2 && l >= fabs((float)(r1-r2))) return 1;
-	return 0;
-} 
+    int l;
+    l = sqrt(powf(fabs((float)(x1 - x2)), 2) + powf(fabs((float)(y1 - y2)), 2));
+    if (l <= r1 + r2 && l >= fabs((float)(r1 - r2)))
+        return 1;
+    return 0;
+}
